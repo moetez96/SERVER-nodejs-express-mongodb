44b1741c068de6228e6ab12af0fd752273d1a2cb
moetez1996 <47902165+moetez1996@users.noreply.github.com>
2021-05-29 16:58:03 +0100

    contacts CRUD

--- routes/contact.js
+++ routes/contact.js
@@ -5,34 +5,71 @@ var Contact = require('../models/contact');
 /* GET contact DB. */
 router.get('/', function (req, res, next) {
     Contact.find(function (err, data) {
-        res.json(data);
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('show', {users: data})
+        }
     });
 });
-/* POST */
-router.post('/', function (req, res, next) {
-    var user = new Contact(
-        {FullName: "Moetez Ayari", Phone: 98000000}
-    );
-    user.save();
-    res.send("Added");
+/* GET Form add new contact. */
+router.get('/add/contact', function (req, res, next) {
+    res.render('add')
+});
+/* GET 1 contact */
+router.get('/:id', function (req, res, next) {
+    Contact.findById(req.params.id, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('detail', {user: data})
+        }
+    });
+});
+/* GET 1 contact for update */
+router.get('/edit/:id', function (req, res, next) {
+    Contact.findById(req.params.id, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('editContact', {user: data})
+        }
+    });
 });
 /* POST 2*/
 router.post('/add', function (req, res, next) {
     new Contact(
         {
-            FullName: req.body.FullName,
-            Phone: req.body.Phone
+            FullName: req.body.fullname,
+            Phone: req.body.phone
         }
     ).save();
-    res.send("Added");
+    res.redirect("/");
+});
+/* Update Contact */
+router.post('/edit/contact/:id', function (req, res, next) {
+    const obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+
+    const myNewContact = {
+        FullName: obj.fullname,
+        Phone: obj.phone
+    }
+    Contact.findByIdAndUpdate(req.params.id, myNewContact, function (err) {
+        if (err) {
+            res.redirect('/edit/contact/' + res.params.id);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 /* Delete contact*/
-router.post('/delete/:id', function (req, res, next) {
+router.get('/delete/:id', function (req, res, next) {
     Contact.findByIdAndRemove(req.params.id,
         function (err, docs) {
             if (err)
                 console.log(err);
-            res.send("contact deleted");
+            res.redirect('/');
         })
 })
 module.exports = router;
